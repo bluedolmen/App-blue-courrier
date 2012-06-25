81b3127957ae35172887f06f2331f60b685d90fa
bpajot <bpajot@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2012-06-25 15:47:53 +0000

    Fixes the implementation to retrieve behaviour parameters
    git-svn-id: http://svn.bluexml.com/private/products/YaMma-EE@37356 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- com.bluexml.alfresco.extjs.yamma-extensions/src/main/resources/alfresco/extension/com/bluexml/alfresco/yamma/policies/onCreateSite.js
+++ com.bluexml.alfresco.extjs.yamma-extensions/src/main/resources/alfresco/extension/com/bluexml/alfresco/yamma/policies/onCreateSite.js
@@ -1,20 +1,29 @@
-///<import resource="classpath:/alfresco/extension/com/bluexml/alfresco/yamma/policies/onCreateSite.js">
+///<import resource="classpath:/alfresco/webscripts/extension/com/bluexml/side/alfresco/extjs/utils/utils.lib.js">
+///<import resource="classpath:/alfresco/extension/com/bluexml/alfresco/yamma/common/yamma-utils.js">
 
 (function() {
+
+	var childAssoc = behaviour.args[0];
+	var node = childAssoc.getChild();	
 	
-	if (!document) {
+	if ('undefined' == typeof node) {
 		logger.warn('[onCreateSite] Cannot find any contextual document.');
 		return;
 	}
-	if (!document.typeShort || 'st:site' != document.typeShort) {
+	
+	if (!node.typeShort || 'st:site' != node.typeShort) {
 		logger.warn('[onCreateSite] The contextual document is not of type st:site as expected');
 		return;
 	}
-	var siteNode = document;
+	var siteNode = node;
 	
 	main();
 	
 	function main() {
+		createSiteTrays();
+	}
+	
+	function createSiteTrays() {
 		TraysUtils.createSiteTrays(siteNode);
 	}
 	
