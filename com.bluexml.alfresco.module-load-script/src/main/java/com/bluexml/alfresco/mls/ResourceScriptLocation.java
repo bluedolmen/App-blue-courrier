d3bdaf021f399cb6e409b3d1c0ce88b1fca4f4a2
bpajot <bpajot@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2012-10-25 16:05:57 +0000

    First release
    git-svn-id: http://svn.bluexml.com/private/products/YaMma-EE@39903 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ com.bluexml.alfresco.module-load-script/src/main/java/com/bluexml/alfresco/mls/ResourceScriptLocation.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (C) 2011 fme AG
+ * 
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.bluexml.alfresco.mls;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import org.alfresco.error.AlfrescoRuntimeException;
+import org.alfresco.scripts.ScriptException;
+import org.alfresco.service.cmr.repository.ScriptLocation;
+import org.springframework.core.io.Resource;
+
+/**
+ * Implements a script location that points to a Spring {@link Resource}.
+ * 
+ * @author Florian Maul (fme AG)
+ */
+public class ResourceScriptLocation implements ScriptLocation {
+
+	private final Resource resource;
+
+	public ResourceScriptLocation(Resource resource) {
+		this.resource = resource;
+	}
+
+	@Override
+	public InputStream getInputStream() {
+		try {
+			
+			return resource.getInputStream();
+			
+		} catch (Throwable err) {
+			
+			throw new ScriptException(
+				String.format("Failed to get resource path '%s': %s", resource, err.getMessage()),
+				err
+			);
+			
+		}
+	}
+
+	@Override
+	public Reader getReader() {
+		try {
+			
+			final InputStream stream = resource.getInputStream();
+			if (stream == null) {
+				throw new AlfrescoRuntimeException(
+					String.format("Unable to load resource: '%s'", resource)
+				);
+			}
+			
+			return new InputStreamReader(stream);
+			
+		} catch (Throwable err) {
+			
+			throw new ScriptException(
+				String.format("Failed to load resource '%s': %s", resource, err.getMessage()),
+				err
+			);
+					
+		}
+
+	}
+
+	@Override
+	public String getPath() {
+		try {
+			
+			return resource.getURI().toString();
+			
+		} catch (Throwable err) {
+			
+			throw new ScriptException(
+				String.format("Failed to get resource path '%s': %s", resource, err.getMessage()),
+				err
+			);
+			
+		}
+	}
+
+	@Override
+	public boolean isCachable() {
+		return true;
+	}
+
+	@Override
+	public boolean isSecure() {
+		return true;
+	}
+
+}
---
+++ com.bluexml.alfresco.module-load-script/src/main/resources/alfresco/webscripts/extension/README.txt
@@ -0,0 +1,4 @@
+Here you can add resources finally located in WEB-INF/classes, like webscripts
+use alfresco/extension/ to override webscript definition,
+or alfresco/webscripts/extension to add new webscript
+  
