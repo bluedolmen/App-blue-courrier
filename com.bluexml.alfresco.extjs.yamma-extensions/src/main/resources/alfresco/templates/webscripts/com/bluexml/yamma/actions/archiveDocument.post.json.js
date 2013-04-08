77766fe867afbdfc929bcc48a4296329c3e6bb02
bpajot <bpajot@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2013-04-08 09:43:12 +0000

    Fixes the order of import which else raises an exception
    git-svn-id: http://svn.bluexml.com/private/products/YaMma-EE@46522 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- com.bluexml.alfresco.extjs.yamma-extensions/src/main/resources/alfresco/templates/webscripts/com/bluexml/yamma/actions/archiveDocument.post.json.js
+++ com.bluexml.alfresco.extjs.yamma-extensions/src/main/resources/alfresco/templates/webscripts/com/bluexml/yamma/actions/archiveDocument.post.json.js
@@ -1,5 +1,5 @@
-///<import resource="classpath:/alfresco/templates/webscripts/com/bluexml/yamma/actions/nodeaction.lib.js">
 ///<import resource="classpath:/alfresco/extension/com/bluexml/alfresco/yamma/common/yamma-env.js">
+///<import resource="classpath:/alfresco/templates/webscripts/com/bluexml/yamma/actions/nodeaction.lib.js">
 
 (function() {
 	
