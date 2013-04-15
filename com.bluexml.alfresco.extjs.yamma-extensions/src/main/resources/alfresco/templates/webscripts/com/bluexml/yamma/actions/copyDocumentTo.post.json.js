b2c632ece14784893604be297ea0623a42ff45f4
bpajot <bpajot@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2013-04-15 10:55:07 +0000

    Restore the document-upload functionality
    git-svn-id: http://svn.bluexml.com/private/products/YaMma-EE@46699 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- com.bluexml.alfresco.extjs.yamma-extensions/src/main/resources/alfresco/templates/webscripts/com/bluexml/yamma/actions/copyDocumentTo.post.json.js
+++ com.bluexml.alfresco.extjs.yamma-extensions/src/main/resources/alfresco/templates/webscripts/com/bluexml/yamma/actions/copyDocumentTo.post.json.js
@@ -22,11 +22,26 @@
 			
 			Yamma.Actions.NodeAction.prepare.call(this);
 			
-			var 
-				destination_ = Utils.asString(this.parseArgs['destination'])
-			;
+			var destination_ = Utils.asString(this.parseArgs['destination']);
 			
-			this.destination = this.extractNode(destination_); 
+			if (Utils.Alfresco.isNodeRef(destination_)) {
+				this.destination = this.extractNode(destination_);
+			} else if (Utils.String.startsWith(destination_, "service://")) {
+				
+				var 
+				  path = destination_.substring(10, destination_.length),
+				  slashIndex = path.indexOf('/'),
+				  serviceName = path.substring(0, slashIndex),
+				  service = siteService.getSite(serviceName),
+				  serviceNode = service.node,
+				  directoryPath = path.substring(slashIndex + 1, path.length)
+				;
+				
+				this.destination = serviceNode.childByNamePath(directoryPath);
+				// TODO: Check the validity of the target
+				
+			}
+			
 			this.typeShort = Utils.asString(this.parseArgs['typeShort']);
 			this.operation = Utils.asString(this.parseArgs['operation']);
 			this.filename = Utils.asString(this.parseArgs['filename']);
